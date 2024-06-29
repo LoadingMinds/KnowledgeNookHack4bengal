900a4584ae319e283f34318f518f37fd4be5b561
bluecoder2003 <neelakshidas2003@gmail.com>
2024-06-29 11:34:57 +0530

    update:progress_tracker

---
+++ src/components/custom/videoaccordion.tsx
@@ -0,0 +1,90 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordian";
+  import { Progress } from "@/components/ui/progress";
+  import VideoCard from "./videocard";
+  import { Dispatch, SetStateAction } from "react";
+  
+  type Video = {
+    index: string;
+    thumbnailUrl: string;
+    videoTitle: string;
+    videoLink: string;
+    channelName: string;
+    views: string;
+    uploadedTime: string;
+    videoTime: string;
+    videoDurationInSeconds: number;
+  };
+
+  type TVideoAccordion = {
+    day: number;
+    videos: number;
+    videosForDay: Video[];
+    checkboxStatus: Record<string, boolean>;
+    setCheckboxStatus: Dispatch<SetStateAction<Record<string, boolean>>>;
+  };
+  
+  export default function VideoAccordion({
+    day,
+    videos,
+    videosForDay,
+    checkboxStatus,
+    setCheckboxStatus,
+  }: TVideoAccordion) {
+    function calculateProgress() {
+      const totalVideos = videosForDay.length;
+      const startIndex = parseInt(videosForDay[0].index);
+      const endIndex = parseInt(videosForDay[videosForDay.length - 1].index);
+      let totalWatchedVideos = 0;
+  
+      for (let i = startIndex; i <= endIndex; i++) {
+        if (checkboxStatus[i.toString()] === true) {
+          totalWatchedVideos++;
+        }
+      }
+  
+      return (totalWatchedVideos / totalVideos) * 100;
+    }
+  
+    return (
+      <div className="mx-auto py-1">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <div className="flex items-center w-full">
+                <h1 className="font-bold text-lg flex-shrink-0">
+                  Day - {day}{" "}
+                  <span className="text-muted-foreground font-normal text-base ml-1 block">
+                    {videos} videos
+                  </span>
+                </h1>
+                <div className="flex-grow mx-4">
+                  <div className="flex justify-center">
+                    <Progress
+                      value={calculateProgress()}
+                      className="w-full md:w-96"
+                    />
+                  </div>
+                </div>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              {videosForDay.map((video) => (
+                <div key={video.index} className="mb-4">
+                  <VideoCard
+                    video={video}
+                    checkboxStatus={checkboxStatus}
+                    setCheckboxStatus={setCheckboxStatus}
+                  />
+                </div>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    );
+  }
---
+++ src/components/custom/videocard.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Dispatch, SetStateAction } from "react";
+
+function capitalizeFirstLetter(str: string): string {
+  return str.replace(/\b\w/g, (match) => match.toUpperCase());
+}
+
+function formatTime(timeString: string): string {
+  const parts = timeString.split(":");
+  const formattedParts = parts.map((part) => {
+    part = part.trim();
+    if (part.length === 1) {
+      return `0${part}`;
+    }
+    return part;
+  });
+
+  return formattedParts.join(":");
+}
+type Video = {
+    index: string;
+    thumbnailUrl: string;
+    videoTitle: string;
+    videoLink: string;
+    channelName: string;
+    views: string;
+    uploadedTime: string;
+    videoTime: string;
+    videoDurationInSeconds: number;
+  };
+type TVideoCard = {
+  video: Video;
+  checkboxStatus: Record<string, boolean>;
+  setCheckboxStatus: Dispatch<SetStateAction<Record<string, boolean>>>;
+};
+
+export default function VideoCard({
+  video,
+  checkboxStatus,
+  setCheckboxStatus,
+}: TVideoCard) {
+  const isChecked = checkboxStatus[video.index];
+
+  const handleCheckboxChange = (index: string) => {
+    setCheckboxStatus((prevStatus) => ({
+      ...prevStatus,
+      [index]: !prevStatus[index],
+    }));
+  };
+
+  return (
+    <div
+      className={`flex border rounded-lg p-4 ${
+        isChecked ? "bg-green-400/60" : ""
+      }`}
+    >
+      <div className="flex w-full items-center justify-between">
+        <a href={video.videoLink} target="_blank" rel="noopener noreferrer">
+          <img
+            src={video.thumbnailUrl}
+            alt={video.videoTitle}
+            className="rounded-lg max-w-fit h-[56px]"
+          />
+        </a>
+        <a
+          href={video.videoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-md font-medium flex-grow truncate hover:underline ml-2"
+        >
+          {capitalizeFirstLetter(video.videoTitle)}
+        </a>
+        <div className="flex items-center gap-2 ml-1">
+          <p className="text-md text-white/60">{formatTime(video.videoTime)}</p>
+          <Checkbox
+            checked={isChecked}
+            onCheckedChange={() => handleCheckboxChange(video.index)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
