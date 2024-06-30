9cf18620a9d0737aacf899f1002067a08be46993
IndraniSom <indranisom39@gmail.com>
2024-06-30 05:44:02 +0530

    asdfghj

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const button = () => {
   return (
-    <div><Link href='/bookcall'>Book a call</Link></div>
+    <div><Link href='/chimp'>Book a call</Link></div>
   )
 }
 
